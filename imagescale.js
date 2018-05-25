36fd2fc45746fa0f3241931e3e443dcaa443cc3b
Sam Su <47660666@qq.com>
2018-05-25 15:01:13 +0800

    dead repo bounce !

--- imagescale.js
+++ imagescale.js
@@ -5,10 +5,19 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var MAX_WIDTH = 1200;
+var new_img_size = 0;
+var reader = new FileReader();
 
 function loadAndDraw(imgFile, quality){
 	img = new Image();
 	img.src = imgFile.result;
+
+	img.onload = function () {
+		console.log("Width:" + this.width + "   Height: " + this.height);
+		if (this.width < MAX_WIDTH) {
+			MAX_WIDTH = this.width;
+		}
+	};
 
 	canvas.width = MAX_WIDTH;
 	canvas.height =  (img.naturalHeight/img.naturalWidth) * MAX_WIDTH;
@@ -18,11 +27,14 @@ function loadAndDraw(imgFile, quality){
 	var ratio  = Math.min ( hRatio, vRatio );
 	var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
 	var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
-	ctx.clearRect(0,0,canvas.width, canvas.height);
-	ctx.drawImage(img, 0,0, img.width, img.height, centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(img, 0, 0, img.width, img.height, centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
 
 	var mimeType = "image/jpeg";
 	var newImageData = canvas.toDataURL(mimeType, quality);
+	img.onload = function () {
+		console.log("Width:" + this.width + "   Height: " + this.height);
+	};
 	return newImageData;
 }
 
@@ -45,17 +57,16 @@ $(document).ready(function(){
 
 	function previewAndUpload(file) {
 		$(".loader-wrap").show();
-		var reader = new FileReader();
 		var imgFile;
 		reader.readAsDataURL(file);
 		reader.onload = function(e){
-			$('.popup').prop('src', this.result);
+			$('#uploader').prop('src', this.result);
 			var i = document.createElement("p");
 			var r = e.target.result.split(";");
 			r = r[0].split("/");
 			r = r[1].split("+");
 			r = r[0].toUpperCase();
-			i.innerHTML = "<br />This is the original <b>" + r + "</b> image, size of <b>" + (e.total / 1024).toFixed(2) + "</b> KB.";
+			i.innerHTML = "<br />This is the original <b>" + r + "</b> image, size of <b>" + (e.total / 1024).toFixed(2) + "</b> kb.";
 			$('.hint').html(i);
 		};
 		reader.onloadend = function(e) {
@@ -65,8 +76,13 @@ $(document).ready(function(){
 
 			imgFile = e.target;
 			var imgscr = loadAndDraw(imgFile, 95);
+			$('#uploader').removeAttr('height');
 			$('#scaledImg').prop('src', imgscr);
 			$('#newImageTitle').css('display', 'block');
+
+			var head = 'data:image/jpeg;base64,';
+			new_img_size = Math.round((imgscr.length - head.length) * 3 / 4 / 1024);
+			$('#new_img_size').html(new_img_size);
 		};
 	}
 });
